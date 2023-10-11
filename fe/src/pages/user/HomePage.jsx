0e7478402559b9033b9f2dce0e91c7df29216fd5
Madanil01 <102971607+Madanil01@users.noreply.github.com>
2023-10-11 11:48:59 +0700

    Update HomePage.jsx

--- fe/src/pages/user/HomePage.jsx
+++ fe/src/pages/user/HomePage.jsx
@@ -46,17 +46,7 @@ const HomePage = () => {
 
   const [activeColor, setActiveColor] = useState(null);
 
-  useEffect(() => {
-    dispatch(getMe());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (isError) {
-      navigate("/");
-    } else if (user && user.role === "admin") {
-      navigate("/restrict");
-    }
-  }, [isError, navigate, user]);
+ 
   // Fungsi untuk mengembalikan semua gambar
   const handleShowAllImages = () => {
     setActiveColor(null); // Set activeColor menjadi null untuk menampilkan semua gambar
