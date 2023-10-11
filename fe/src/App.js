8b370c81de88a31651407c25c5fabc96404c1cbe
Madanil01 <102971607+Madanil01@users.noreply.github.com>
2023-10-11 11:19:58 +0700

    Update App.js

--- fe/src/App.js
+++ fe/src/App.js
@@ -22,7 +22,7 @@ import CheckOut from './pages/user/CheckOut';
 function App() {
   return (
     <div>
-      <BrowserRouter basename="/react">
+      <BrowserRouter basename="/fe/src/App.js">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
