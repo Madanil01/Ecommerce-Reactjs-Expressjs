f46df8444a46d95d97cfeb43a4c5732c89184718
Madanil01 <102971607+Madanil01@users.noreply.github.com>
2023-10-18 23:15:13 +0700

    Update Database.js

--- be/config/Database.js
+++ be/config/Database.js
@@ -1,14 +1,17 @@
-import { Sequelize } from "sequelize";
+import Sequelize from "sequelize";
+import mysql2 from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.NEXT_PUBLIC_DB_NAME, // Sesuaikan nama variabel lingkungan dengan "NEXT_PUBLIC_"
-  process.env.NEXT_PUBLIC_DB_USER, // Sesuaikan nama variabel lingkungan dengan "NEXT_PUBLIC_"
-  process.env.NEXT_PUBLIC_DB_PASSWORD, // Sesuaikan nama variabel lingkungan dengan "NEXT_PUBLIC_"
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.NEXT_PUBLIC_DB_HOST, // Sesuaikan nama variabel lingkungan dengan "NEXT_PUBLIC_"
-    dialect: 'mysql2',
+    dialect: "mysql",
+    dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
   }
 );
 
