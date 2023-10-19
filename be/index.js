af94c918a1d16904b74af17b2d5c177dc7924748
Madanil01 <102971607+Madanil01@users.noreply.github.com>
2023-10-19 14:01:04 +0700

    Update index.js

--- be/index.js
+++ be/index.js
@@ -7,7 +7,7 @@ import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from "./routes/ProductRoute.js"; 
 import CategoryRoute from "./routes/CategoryRoute.js"; 
-import VariasiRoute from "./routes/VariasiRoute.js";
+import VariasiRoute from "./routes/VariasiRoute.js"; 
 import ColorRoute from "./routes/ColorRoute.js";
 import MemoryRoute from "./routes/MemoryRoute.js";
 import PesananRoute from "./routes/PesananRoute.js";
