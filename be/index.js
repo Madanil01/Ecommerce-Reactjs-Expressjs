bf9f6a73aa37b71a24b6889c0eba0e157483e576
Madanil01 <102971607+Madanil01@users.noreply.github.com>
2023-10-19 11:34:47 +0700

    Update index.js

--- be/index.js
+++ be/index.js
@@ -61,7 +61,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://main--tiny-cuchufli-c4e6c1.netlify.app"); // Set the origin to your client's URL
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  //res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
   next();
 });
