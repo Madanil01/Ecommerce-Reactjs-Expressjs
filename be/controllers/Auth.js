76632ed2167e6ed8c812065993909496bd6fb995
Madanil01 <102971607+Madanil01@users.noreply.github.com>
2025-02-27 10:52:41 +0700

    Update Auth.js

--- be/controllers/Auth.js
+++ be/controllers/Auth.js
@@ -11,7 +11,7 @@ export const Login = async (req, res) => {
   const match = await argon2.verify(user.password, req.body.password);
   if (!match) return res.status(400).json({ msg: "Wrong Password" });
   req.session.userId = user.uuid;
-  console.log(req.session)
+  console.log(req.session.userId)
   const uuid = user.uuid;
   const name = user.name;
   const email = user.email;
